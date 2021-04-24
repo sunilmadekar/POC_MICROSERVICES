53d3eb8733144bb60c523a368c1dd123d198461a
sunil <Sunil.Madekar@gmail.com>
2021-04-24 23:09:24 +0530

    College base code

---
+++ college/src/main/java/com/smware/college/controller/CollegePojo.java
@@ -0,0 +1,31 @@
+package com.smware.college.controller;
+
+public class CollegePojo 
+{
+    private String name;
+    private String address;
+     
+    public CollegePojo(String name, String address) {
+        super();
+        this.name = name;
+        this.address = address;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+ 
+  
+}
---
+++ college/src/test/java/com/smware/college/CollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smware.college;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
