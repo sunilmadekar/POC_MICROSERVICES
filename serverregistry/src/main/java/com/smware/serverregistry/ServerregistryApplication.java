f6f84edd961df4ec61e037eb30ea503b8aa11af4
sunil <Sunil.Madekar@gmail.com>
2021-04-24 20:13:28 +0530

    service registry base project

---
+++ serverregistry/src/main/java/com/smware/serverregistry/ServerregistryApplication.java
@@ -0,0 +1,13 @@
+package com.smware.serverregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerregistryApplication.class, args);
+	}
+
+}
